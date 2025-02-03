daf7148d996c85b85e18405d3355ff94543e3106
sauvi9non <xxxdmstj@gmail.com>
2025-02-03 11:52:57 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/example/nomad_thread/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nomad_thread
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NomadThreadApp());
+}
+
+class NomadThreadApp extends StatelessWidget {
+  const NomadThreadApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
