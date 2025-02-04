e9379c16e66ad79cc8c2a42d84020fd2897e394b
sauvi9non <xxxdmstj@gmail.com>
2025-02-04 15:17:24 +0900

    [DOCS] json 데이터 추가, uuid 패키지 설치

--- lib/icon_nav.dart
+++ lib/icon_nav.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:nomad_thread/constants/sizes.dart';
 
 class IconNav extends StatelessWidget {
   final IconData icon;
